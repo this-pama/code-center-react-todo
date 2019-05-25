800f3c7328a83cc030ddf6f853f6492770c57fcb
Dapo Aderemi <adedapopaul@yahoo.com>
2019-05-24 22:14:36 -0700

    handle routing

--- app/components/sigin.js
+++ app/components/sigin.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Switch, Route, withRouter, Link }from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { SiginView } from './view/signin';
+import { SignUp } from './signup';
+import { App } from './App';
 
 export class SignIn extends React.Component{
     constructor(props){
@@ -6,13 +11,22 @@ export class SignIn extends React.Component{
         this.state= {
             username: '',
             password: '',
-            disable: true
+            disable: true,
+            errMessage: '',
+            isSignIn: true,
+            isHome: false
         }
 
         this.handleUsername = this.handleUsername.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
         this.validateForm = this.validateForm.bind(this)
+        this.logIn = this.logIn.bind(this)
     }
+
+    conmponentDidMount(){
+        this.props.isSignIn = this.state.isSignIn
+    }
+
      async logIn (username, password){
         var url = "https://code-center-express-app.herokuapp.com/login"
        var loginFetch = await fetch(url,{
@@ -23,18 +37,42 @@ export class SignIn extends React.Component{
                 password
             }),
         })
+        var response = await loginFetch
+        var res = await response.json()
+        console.log(res)
+        if(res.username){
+            console.log("Log in successful")
+            this.setState({
+                isHome : true,
+                errMessage: '',
+                isSignIn: false
+            })
+            return <App />
+        }
+        else if(res.text){
+            console.log(res.text);
+            this.setState({
+                errMessage : res.text
+            })
+            
+        }
+        else{
+            this.setState({
+                errMessage: "Error Occurred."
+            })
+        }
     }
 
-    handleUsername(username){
+    handleUsername = username =>{
         if(username.target.value.length > 0){
             this.setState({
                 username: username.target.value
             }, this.validateForm)
-            console.log(username.target.value)
+            // console.log(username.target.value)
         }
         else{
             this.setState({
-                errMessage: "Password cannot be empty"
+                errMessage: "Username cannot be empty"
             })
         }
     }
@@ -44,7 +82,7 @@ export class SignIn extends React.Component{
             this.setState({
                 password: password.target.value
             }, this.validateForm)
-            console.log(password.target.value)
+            // console.log(password.target.value)
         }
         else{
             this.setState({
@@ -63,13 +101,23 @@ export class SignIn extends React.Component{
 
 
     render(){
-        return(
-            <form>
-                <p>{this.state.errMessage}</p>
-                <input placeholder="Username" name='username' type='text' value={this.state.username}  onChange= {this.handleUsername}/> <br />
-                <input placeholder='Password' type='password' name='password' value={this.state.password} onChange= {this.handlePassword} /> <br />
-                <button onClick= {()=> this.logIn(this.state.username, this.state.password) } disabled={ this.state.disable}>Log In</button>
-            </form>
-        )
+        if(this.state.isSignIn){
+            return <SiginView 
+                password={ this.state.password}
+                username= {this.state.username}
+                disable= { this.state.disable}
+                handlePassword= {this.handlePassword}
+                handleUsername = {this.handleUsername}
+                logIn= { this.logIn}
+                errMessage= { this.state.errMessage}
+                />
+        }
+        else if (this.state.isHome){
+            return <App />
+        }
+        else{
+            return <SignUp />
+        }
+        
     }
 }
