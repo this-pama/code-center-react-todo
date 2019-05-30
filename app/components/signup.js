dae930cbc92b54aa5e3340085e80de8e68db57ed
Dapo Aderemi <adedapopaul@yahoo.com>
2019-05-30 10:11:10 -0700

    add signup page

--- app/components/signup.js
+++ app/components/signup.js
@@ -1,21 +1,166 @@
 import React from 'react'
+import {SignUpView } from './view/signup'
+import { App } from './App';
+
 
 export class SignUp extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            firstName: '',
+            lastName:'',
+            userName: '',
+            password: '',
+            bio: '',
+            email: '',
+            gender: '',
+            phoneNumber: '' ,
+            address: '',
+            stateOfOrigin: '',
+
+        }
+        
+    }
+
+    handleFirstName= (firstName)=> {
+        if(firstName.target.value.length > 0){
+            this.setState({
+                firstName: firstName.target.value 
+            })
+        }
+    }
+
+    handleLastName= (name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                lastName: name.target.value 
+            })
+        }
+    }
+
+    handleUsername =(name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                userName: name.target.value 
+            })
+        }
+    }
+
+    handlePassword =(name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                password: name.target.value 
+            })
+        }
+    }
+
+    handleBio =(name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                bio: name.target.value 
+            })
+        }
+    }
+
+    handleEmail = (name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                email: name.target.value 
+            })
+        }
+    }
+
+    handleGender =(name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                gender: name.target.value 
+            })
+        }
+    }
+
+    handleAddress =(name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                address: name.target.value 
+            })
+        }
+    }
+
+    handlePhoneNumber = (name ) => {
+        if(name.target.value.length > 0){
+            this.setState({
+                phoneNumber: name.target.value 
+            })
+        }
+    }
+
+    handleStateOfOrigin  =(name) =>{
+        if(name.target.value.length > 0){
+            this.setState({
+                stateOfOrigin: name.target.value 
+            })
+        }
+    }
+
+    signUp =  async () => {
+        var url = "https://code-center-express-app.herokuapp.com/signup"
+       var signUpFetch = await fetch(url,{
+            method: "POST",
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({
+                firstName: this.state.firstName,
+                lastName:this.state.lastName,
+                userName: this.state.userName,
+                password: this.state.password,
+                bio: this.state.bio,
+                email: this.state.email,
+                gender: this.state.gender,
+                phoneNumber: this.state.phoneNumber,
+                address: this.state.address,
+                stateOfOrigin: this.state.stateOfOrigin,
+                
+            }),
+        })
+        var response = await signUpFetch
+        var res = await response.json()
+        console.log(res)
+        if(res.err){
+            this.props.errMessage = "There seems to be an error"
+        }
+        else if (res.userName){
+            return <App />
+        }
+        else{
+            this.props.errMessage = "Network Error"
+        }
+    
+    }
+
     render(){
         return(
-            <form>
-                <input placeholder='First Name' type='text' name='firstName' /> <br />
-                <input placeholder='Last Name' type='text' name='lastName' /> <br />
-                <input placeholder='Username' type='text' name='userName' /> <br />
-                <input placeholder='Password' type='password' name='password' /> <br />
-                <input placeholder='Bio...' type='text' name='bio' /> <br />
-                <input placeholder='Email' type='text' name='email' /> <br />
-                <input placeholder='Gender' type='text' name='gender' /> <br />
-                <input placeholder='Phone Number' type='text' name='phoneNumber' /> <br />
-                <input placeholder='Address' type='text' name='address' /> <br />
-                <input placeholder='State' type='text' name='state' />  <br />
-                <button >Sign Up</button> <br />
-            </form>
+           <SignUpView 
+            firstName= { this.state.firstName}
+            lastName = {this.state.lastName}
+            userName= {this.state.userName}
+            password= {this.state.password}
+            bio={this.state.password}
+            email= {this.state.email}
+            gender = {this.state.gender}
+            phoneNumber ={this.state.phoneNumber}
+            address= {this.state.address}
+            stateOfOrigin= {this.state.stateOfOrigin}
+            handleFirstName= {this.handleFirstName}
+            handleLastName ={this.handleLastName}
+            handleUsername= {this.handleUsername}
+            handlePassword= {this.handlePassword}
+            handleBio= {this.handleBio}
+            handleEmail= {this.handleEmail}
+            handleGender= {this.handleGender}
+            handlePhoneNumber= {this.handlePhoneNumber}
+            handleAddress= {this.handleAddress}
+            handleStateOfOrigin= {this.handleStateOfOrigin}
+            signUp= {this.signUp}
+           />
         )
     }
 }
---
+++ app/components/view/signup.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BrowserRouter as Router } from "react-router-dom";
+
+export class SignUpView extends React.Component{
+    render(){
+        return(
+            <Router>
+                <div>
+                    <p style={{color: 'red'}}>{this.props.errMessage}</p>
+                    <input placeholder='First Name' 
+                            type='text' name='firstName' 
+                            value={this.props.firstName} 
+                            onChange={this.props.handleFirstName} 
+                    /> <br />
+                    <input placeholder='Last Name' 
+                            type='text' name='lastName' 
+                            value={this.props.lastName} 
+                            onChange={this.props.handleLastName} 
+                    /> <br />
+                    <input placeholder='Username'
+                            type='text' name='userName' 
+                            value={this.props.userName} 
+                            onChange={this.props.handleUsername}
+                    /> <br />
+                    <input placeholder='Password' 
+                            type='password'  
+                            value={this.props.password}  
+                            onChange={this.props.handlePassword}
+                    /> <br />
+                    <input placeholder='Bio...' 
+                            type='text' name='bio' 
+                            value={this.props.bio} 
+                            onChange={this.props.handleBio} 
+                    /> <br />
+                    <input placeholder='Email' 
+                            type='text' 
+                            value={this.props.email} 
+                            onChange={this.props.handleEmail} 
+                    /> <br />
+                    <input placeholder='Gender' 
+                            type='text' 
+                            value={this.props.gender} 
+                            onChange={this.props.handleGender} 
+                    /> <br />
+                    <input placeholder='Phone Number' 
+                            type='number'
+                            value={this.props.phoneNumber} 
+                            onChange={this.props.handlePhoneNumber} 
+                    /> <br />
+                    <input placeholder='Address' 
+                            type='text'  
+                            value={this.props.address} 
+                            onChange={this.props.handleAddress} 
+                    /> <br />
+                    <input placeholder='State' 
+                            type='text'
+                            value={this.props.stateOfOrigin} 
+                            onChange={this.props.handleStateOfOrigin} 
+                    />  <br />
+                    <button 
+                        onClick={()=> this.props.signUp} 
+                    >Sign Up</button> <br />
+                </div>
+            </Router>
+        )
+    }
+}
+
+
+SignUpView.propTypes = {
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    gender: PropTypes.string.isRequired,
+    phoneNumber: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    stateOfOrigin: PropTypes.string.isRequired,
+    handleFirstName: PropTypes.func.isRequired,
+    handleLastName: PropTypes.func.isRequired,
+    handleUsername: PropTypes.func.isRequired,
+    handlePassword: PropTypes.func.isRequired,
+    handleBio: PropTypes.func.isRequired,
+    handleEmail: PropTypes.func.isRequired,
+    handleGender: PropTypes.func.isRequired,
+    handlePhoneNumber: PropTypes.func.isRequired,
+    handleAddress: PropTypes.func.isRequired,
+    handleStateOfOrigin: PropTypes.func.isRequired,
+    signUp: PropTypes.func.isRequired,
+    errMessage: PropTypes.string
+
+}
